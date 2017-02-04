ecbd96964220dfbf6ff2efecea0dfcf2f2cd9323
John Nagle <nagle@sitetruth.com>
2017-02-04 10:33:09 -0800

    New indexing scheme

--- src/Util/picfix.py
+++ src/Util/picfix.py
@@ -17,10 +17,12 @@ UNICODECORNERS = "┏┓┗┛"
 UNICODETEES = "┣┫┻┳"
 UNICODELINES = "┃━"
 UNICODEARROWS = "▶◀▲▼"
+UNICODELDC = UNICODECORNERS + UNICODETEES + UNICODELINES + UNICODEARROWS
 #
 #   Globals
 #
 verbose = False                 # verbose mode
+   
 
 #
 #   class Linegroup -- group of three lines handled together
@@ -30,17 +32,54 @@ class Linegroup :
         self.outf = outf                # output goes here
         self.lines = [None, None,None]  # last three lines
         
-    def addline(self, s) :              # add and process one line
-        if self.lines[0] is not None :
-            self.outf.write(self.lines[0])
-        self.lines[0] = self.lines[1]   # shift lines by 1
+    def getc(self, row, col) :
+        """
+        Get char at given row and column.
+        Return space if column out of range or row not loaded yet.
+        This makes everything look as if surrounded by whitespace.
+        """
+        if (self.lines[row] is None) :      # off the top
+            return(" ")
+        if col < 0 or col >= len(self.lines[row]) :
+            return(" ")                     # off the end
+        ch = self.lines[row][col]           # get char
+        if ch == '\n' :                     # make end of line
+            ch = " "                        # look like space
+        return(ch)                          # normal case
+        
+                
+    def fixchar(self, i) :
+        """
+        Fix one char in the middle line
+        """
+        #   Horizontals
+        ch = self.getc(1,i)
+        if ch not in "_-|+*<>^V" :
+            return(ch)                      # uninteresting
+        return("█")                         # ***TEMP****
+        
+    def fixline(self) :
+        """
+        Use Unicode line drawing symbols, examining a 3x3 square
+        around the character of interest.  There is an extra space
+        at the beginning and end of the line, to simplify indexing.
+        The prevous line has already been processed.
+        """
+        s = ''
+        for i in range(len(self.lines[1])) :# for all on line
+            s += self.fixchar(i)            # do one char
+        return(s)
+        
+    def addline(self, s) :                  # add and process one line
+        self.lines[0] = self.lines[1]       # shift lines up by 1
         self.lines[1] = self.lines[2]
         self.lines[2] = s
+        if self.lines[0] is not None :      # if all 3 lines are full
+            s = self.fixline()              # fix middle line based on adjacent info
+            self.outf.write(s.rstrip() + '\n')  # output line       
     
-    def flush(self) :                   # call at end to flush last line
-        for i in range(2) :
-            if self.lines[i] is not None :
-                self.outf.write(self.lines[i])
+    def flush(self) :                       # call at end to flush last line
+        self.addline("")                    # force last line out
         
 
 #
